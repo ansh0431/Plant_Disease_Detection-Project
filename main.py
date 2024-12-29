4e7831305f271879dda8fbb4f5389c56d01165f9
ansh0431 <ansh31ay@gmail.com>
2024-12-29 19:03:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if app_mode == "Home":
     image_path = "Home_page.jpg"
     st.image(image_path, use_container_width=True)
     st.markdown("""
-    Welcome to the Cotton Leaf Disease Detection System! 🌿🔍
+    Welcome to the Plant Disease Detection System! 🌿🔍
     
     Our mission is to help in identifying cotton crop diseases efficiently. Upload an image, and our system will analyze it to detect any signs of diseases. Together, let's protect our cotton crops and ensure a healthier harvest!
 
@@ -47,14 +47,14 @@ elif app_mode == "About":
     st.markdown("""
     #### About Dataset
     This dataset is recreated using offline augmentation from the original dataset. The original dataset can be found on this GitHub repo.
-    This dataset consists of about 7.8K RGB images of healthy and diseased crop leaves, categorized into 9 different classes. The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
+    This dataset consists of about 7.8K RGB images of healthy and diseased crop leaves, categorized into 38 different classes. The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
     
     #### Content
     1. Train (6251 images)
     2. Validation (1563 images)
 
 
-    #### Developed by Akshat, Himanshu, Minav, Vedansh 
+    #### Developed by Ansh
     """)
 
 elif app_mode == "Disease Recognition":
@@ -72,16 +72,42 @@ elif app_mode == "Disease Recognition":
             result_index = model_prediction(test_image)
             
             # Reading Labels
-            class_name = [
-                'Healthy', 
-                'Infected-Aphids', 
-                'Infected-Army worm', 
-                'Infected-Bacterial Blight', 
-                'Infected-Cotton Boll Rot', 
-                'Infected-Curl Virus', 
-                'Infected-Fusarium Wilt', 
-                'Infected-Powdery mildew', 
-                'Infected-Target Spot'
-            ]
-            
+            class_name =['Apple___Apple_scab',
+                         'Apple___Black_rot',
+                         'Apple___Cedar_apple_rust',
+                         'Apple___healthy',
+                         'Blueberry___healthy',
+                         'Cherry_(including_sour)___Powdery_mildew',
+                         'Cherry_(including_sour)___healthy',
+                         'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                         'Corn_(maize)___Common_rust_',
+                         'Corn_(maize)___Northern_Leaf_Blight',
+                         'Corn_(maize)___healthy',
+                         'Grape___Black_rot',
+                         'Grape___Esca_(Black_Measles)',
+                         'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                         'Grape___healthy',
+                         'Orange___Haunglongbing_(Citrus_greening)',
+                         'Peach___Bacterial_spot',
+                         'Peach___healthy',
+                         'Pepper,_bell___Bacterial_spot',
+                         'Pepper,_bell___healthy',
+                         'Potato___Early_blight',
+                         'Potato___Late_blight',
+                         'Potato___healthy',
+                         'Raspberry___healthy',
+                         'Soybean___healthy',
+                         'Squash___Powdery_mildew',
+                         'Strawberry___Leaf_scorch',
+                         'Strawberry___healthy',
+                         'Tomato___Bacterial_spot',
+                         'Tomato___Early_blight',
+                         'Tomato___Late_blight',
+                         'Tomato___Leaf_Mold',
+                         'Tomato___Septoria_leaf_spot',
+                         'Tomato___Spider_mites Two-spotted_spider_mite',
+                         'Tomato___Target_Spot',
+                         'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                         'Tomato___Tomato_mosaic_virus',
+                         'Tomato___healthy']
             st.success(f"Model is Predicting it's a {class_name[result_index]}")
